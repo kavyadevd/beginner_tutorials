fdbb8a6184123021a76fd0ef0a808c74caa2fead
Kavya D <kavya27.devadiga@gmail.com>
2021-11-12 16:37:26 -0500

    Add test case for change service message method

--- test/TalkerListenerTest.cpp
+++ test/TalkerListenerTest.cpp
@@ -14,3 +14,15 @@ TEST(TestSimulatorStub, TestPublisherExists) {
     ros::Subscriber test_subscriber = nh.subscribe("talker", 1000, chatterCallback);
     EXPECT_TRUE(exists(test_subscriber));
 }
+
+/**
+ * @brief Tests whether the changeBaseString service can modify the output message
+ */
+TEST(TestSimulatorStub, TestMessageChangeService) {
+  auto client = nh.serviceClient
+  <beginner_tutorials::ServiceFile>("ServiceFile");
+  beginner_tutorials::ServiceFile service_object;
+  service_object.request.inString = "Service test in progress";
+  client.call(service_object.request, service_object.response);
+  EXPECT_STREQ("Service test in progress", string(service_object.response.outString));
+}
